26b3f1b05b2fa2e23a3407f85651069cdf46e6c0
kugis000 <58030143+kugis000@users.noreply.github.com>
2021-02-16 12:39:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.S4GuCHwWwJ_U2l-wOs_hUKToPruYECnn);//BOT_TOKEN is the Client Secret
+client.login(process.env.TOKEN);//BOT_TOKEN is the Client Secret
